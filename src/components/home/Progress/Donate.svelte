5c7a10b3433926d40c23699153ebfa7f6979c7ac
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-03 00:41:41 +0700

    change:color and allightment

--- src/components/home/Progress/Donate.svelte
+++ src/components/home/Progress/Donate.svelte
@@ -3,4 +3,4 @@
 	export let percentprogress: number; // Declare percentprogress as a prop    let percent = 0;
 </script>
 
-<Progressbar progress={percentprogress} size="h-5 md:h-7" />
+<Progressbar progress={percentprogress} size="h-5 md:h-7" color="green"  />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,20 @@
 	let readableTotalDonated = numberWithCommas(totalDonated);
 	let totalLeft = totalRequired - totalDonated;
 	PercentProgress = (totalDonated / totalRequired) * 100;
+	let colorcode = "#00a99d"
 	function numberWithCommas(x: number): string {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	}
 </script>
 
 <div class="mx-auto grid w-full max-w-4xl grid-cols-1 space-y-2 rounded-lg bg-gray-200 shadow-lg">
-	<div class="mx-auto mt-2 h-fit w-full rounded-lg bg-white pb-9 shadow-lg">
-		<div class="bg-amber-400">
-			<h1 class="mx-auto py-2 text-center text-4xl font-semibold">HBD AISHA PROJECT 2024</h1>
+	<div class="mx-auto md:mt-2 h-fit w-full rounded-lg bg-white pb-9 shadow-lg">
+		<div class="">
+			<h1 class="mx-auto py-2 text-center text-4xl font-semibold text-aisha">HBD AISHA PROJECT 2024</h1>
 			<Corousel />
 		</div>
 		<div>
-			<h2 class="mb-4 mt-8 text-center text-3xl font-semibold">PROJECT OVERALL</h2>
+			<h2 class="mb-2 mt-4 text-center text-3xl font-semibold text-aisha">PROJECT OVERALL</h2>
 			<img class="mx-auto h-auto w-[80%]" src="/Overall/30novComing.webp" alt="project overall" />
 			<p class="text-md mx-auto px-2 py-2 text-center">
 				โดยที่ประชาชนแห่งสหประชาชาติได้ยืนยันอีกครั้งไว้ในกฎบัตรถึงศรัทธาในสิทธิมนุษยชนขั้นพื้นฐานในศักดิ์ศรีและค่าของมนุษย์และในสิทธิ
@@ -30,9 +31,9 @@
 				และอิสรภาพขั้นพื้นฐานโดยสากลโดยความร่วมมือกับสหประชาชาติ
 			</p>
 		</div>
-		<div class="mx-auto mt-2 bg-amber-400 px-4">
+		<div class="mx-auto mt-2 b px-4">
 			<Progress percentprogress={PercentProgress} />
-			<h2 class="  text-center text-2xl font-semibold">WE are {PercentProgress}% until complete</h2>
+			<h2 class="  text-center text-2xl font-semibold text-aisha">We are {PercentProgress}% until complete</h2>
 			<div class="mx-auto text-lg">
 				<small class="mx-auto -mt-1 block text-center"
 					>ยอดสมทบทุนตอนนี้มี {readableTotalDonated} บาท</small
@@ -57,8 +58,8 @@
 			</div>
 		</div>
 		<div class="mx-2 -mt-6">
-			<hr class="mt-8 h-px border-0 bg-gray-300" />
-			<h2 class="text-center text-5xl font-semibold">Come and join us!</h2>
+			<hr class="mt-8 h-px border-aisha bg-gray-300 " />
+			<h2 class="text-center text-5xl font-semibold text-aisha">Come and join us!</h2>
 		</div>
 	</div>
 </div>
