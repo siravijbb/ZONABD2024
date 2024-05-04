d1f43dcb66eeff26236c6380a7dc30940a50cf7f
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-04 19:08:25 +0700

    fix:not working on IOS

--- src/components/home/Donate/Progress/Donate.svelte
+++ src/components/home/Donate/Progress/Donate.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
 	import { Progressbar } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
 	export let percentprogress: number; // Declare percentprogress as a prop    let percent = 0;
 	let percentlimit = Math.trunc(percentprogress);
+	let showdonate = 'hidden';
+	let sizemd = 0;
 	/**
 	 * 0 36 90 x
 	 * 0 43 92
 	 * 0 46 95
 	 *
 	 */
-	let sizexs = Math.trunc(90 * (percentlimit / 100));
-	let sizesm = Math.trunc(92 * (percentlimit / 100));
-	let sizemd = Math.trunc(95 * (percentlimit / 100));
+	onMount(async () => {
+		sizemd = Math.trunc(95 * (percentlimit / 100));
+		showdonate = '';
+	});
 </script>
 
-<div class="mt-10 w-full overflow-y-visible md:mt-20">
+<div class="mt-10 w-full overflow-y-visible md:mt-20 {showdonate}">
 	<Progressbar progress={percentprogress} size="h-5 md:h-7" color="green" />
 	<div class="relative -ml-5 overflow-y-visible sm:-ml-0 md:-ml-5">
 		<img
 			src="/Overall/DonateGoal/rocket.png"
 			alt="Goal!"
-			class=" absolute h-14 md:h-24 lg:h-28 ml-[{sizemd}%] -mt-16 overflow-y-visible md:-mt-[105px] lg:-mt-[120px]"
+			class=" absolute h-14 md:h-24 lg:h-28 ml-[{sizemd}%] md:ml-[{sizemd}%] -mt-16 overflow-y-visible md:-mt-[105px] lg:-mt-[120px]"
 		/>
 	</div>
 </div>
