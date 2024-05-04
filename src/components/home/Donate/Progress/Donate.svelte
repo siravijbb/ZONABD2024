ddf84cdcca7afaf8c5e1dac80cb0aa4e3d13054c
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-04 20:39:01 +0700

    fix:this might help

--- src/components/home/Donate/Progress/Donate.svelte
+++ src/components/home/Donate/Progress/Donate.svelte
@@ -11,18 +11,25 @@
 	 *
 	 */
 
-	sizemd = 44;
+	sizemd = 22;
+	let mlnumber = (percentprogress * 0.92) + '%';
 
 </script>
 
 <div class="relative my-10 w-full overflow-y-visible md:my-20 ">
 	<Progressbar progress={percentprogress} size="h-5 md:h-7 w-full" color="green" />
-	<div class=" -ml-5 overflow-y-visible sm:-ml-0 md:-ml-5 w-full">
+	<div class=" -ml-5 overflow-y-visible sm:-ml-0 md:-ml-5 w-full" style="--theme-color: {mlnumber}">
 		<img
 			src="/Overall/DonateGoal/rocket.png"
 			alt="Goal!"
-			class=" relative h-14 md:h-24 lg:h-28 ml-[{sizemd}%] -mt-16 overflow-y-visible md:-mt-[105px] lg:-mt-[120px]"
+			class=" relative h-14 md:h-24 lg:h-28  -mt-16 overflow-y-visible md:-mt-[105px] lg:-mt-[120px]"
 		/>
 	</div>
-	{sizemd}
 </div>
+
+
+<style>
+	img {
+		margin-left: var(--theme-color);
+	}
+</style>
