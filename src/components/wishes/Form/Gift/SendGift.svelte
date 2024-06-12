8cb1b0c984f0c5bdea7cc54a864f7cfe3c261bb4
Siravij Praevisavakij <siravijbb@gmail.com>
2024-06-12 23:16:33 +0700

    remove:img change colr

--- src/components/wishes/Form/Gift/SendGift.svelte
+++ src/components/wishes/Form/Gift/SendGift.svelte
@@ -22,44 +22,36 @@
 		/>
 		<label
 			for="hosting-small"
-			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-aisha  p-5 text-white hover:bg-[#bb433d]/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:peer-checked:text-blue-500"
+			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-aisha  p-5 text-white hover:bg-aisha/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:peer-checked:text-blue-500"
 		>
-			<div class="mx-auto">
-				<img alt="Hoku " src="/favicon.png" class="-my-7 mx-auto -mt-9 flex h-auto w-28" />
-			</div>
+
 		</label>
 	</li>
 	<li>
 		<input type="radio" id="hosting-big" name="color" value="2" class="peer" bind:group={gift} />
 		<label
 			for="hosting-big"
-			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#22b573] p-5 text-white hover:bg-blue-600/30 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:peer-checked:text-blue-500"
+			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#22b573] p-5 text-white hover:bg-[#22b573]/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:peer-checked:text-blue-500"
 		>
-			<div class="mx-auto">
-				<img alt="Idol " src="/favicon.png" class="-my-7 mx-auto -mt-9 flex h-auto w-28" />
-			</div>
+
 		</label>
 	</li>
 	<li>
 		<input type="radio" id="hosting-3" name="color" value="3" class="peer" bind:group={gift} />
 		<label
 			for="hosting-3"
-			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#0071bc] p-5 text-gray-500 hover:bg-[#fffafa]/30 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
+			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#0071bc] p-5 text-gray-500 hover:bg-[#0071bc]/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
 		>
-			<div class="mx-auto">
-				<img alt="Winter Wonder " src="/favicon.png" class="-my-7 mx-auto -mt-9 flex h-auto w-28" />
-			</div>
+
 		</label>
 	</li>
 	<li>
 		<input type="radio" id="hosting-4" name="color" value="4" class="peer" bind:group={gift} />
 		<label
 			for="hosting-4"
-			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#fbb03b] p-5 text-gray-500 hover:bg-[#bb433d]/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
+			class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-[#fbb03b] p-5 text-gray-500 hover:bg-[#fbb03b]/70 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-gray-300 dark:peer-checked:text-blue-500"
 		>
-			<div class="mx-auto">
-				<img alt="3 Year Anni " src="/favicon.png" class="-my-8 mx-auto -mt-8 flex h-auto w-28" />
-			</div>
+
 		</label>
 	</li>
 </ul>
