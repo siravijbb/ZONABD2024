fbcb80e44c256acd8f410fe0b8360bba8b2a348b
Siravij Praevisavakij <siravijbb@gmail.com>
2024-06-17 00:48:52 +0700

    idl

--- src/components/wishes/Header/HBDwithCoro.svelte
+++ src/components/wishes/Header/HBDwithCoro.svelte
@@ -3,7 +3,7 @@ import Corousel from "$components/home/Corousel/Corousel.svelte";
 </script>
 
 <div class="">
-	<h1 class="mx-auto pt-2 text-center text-4xl font-semibold text-aisha HEADER">HAPPY BIRTH DAY AISHA 2024</h1>
+	<h1 class="mx-auto pt-2 text-center text-3xl font-semibold text-aisha HEADER">HAPPY BIRTH DAY AISHA 2024</h1>
 	<h1 class="mx-auto -mt-1 pb-2 text-center text-2xl HEADER">29 July 2024</h1>
 	<Corousel />
 </div>
--- src/components/wishes/Header/Header.svelte
+++ src/components/wishes/Header/Header.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="">
-	<h1 class="mx-auto pt-2 text-center text-4xl font-semibold text-aisha HEADER">AISHA BIRTHDAY WISHES</h1>
+	<h1 class="mx-auto pt-2 text-center text-3xl font-semibold text-aisha HEADER">AISHA BIRTHDAY WISHES</h1>
 	<h1 class="mx-auto -mt-1 pb-2 text-center text-2xl HEADER">ร่วมเขียนคำอวยพรให้ไอช่า</h1>
 </div>
 <style>
