6266f282db03d77900d468f9442631f4963ed62c
Siravij Praevisavakij <Siravijbb@gmail.com>
2024-06-25 19:57:50 +0700

    add:credit

--- src/components/wishes/Credit/Credit.svelte
+++ src/components/wishes/Credit/Credit.svelte
@@ -2,6 +2,7 @@
     หมดแล้ว มุแง!
     <p class="mx-auto text-lg text-zona-SLIVER text-center font-bold" id="credit">Credit:</p>
     <p>DEV: สิราวิ้จจะบึ</p>
+    <p>Consult: Le'Beau</p>
     <p>ART: @nachIInatcha</p>
     <p>ART And Website CMS: Setsugi</p>
     <p>Project Approval: Polygon Project</p>
